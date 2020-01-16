249106b87122de958c711d9c74c5e46ee6d1c80d
Mark Alston <malston@pivotal.io>
2020-01-16 13:23:57 -0700

    Add helper scripts for docker image

---
+++ scripts/build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+docker_image_name="${1:-"malston/bosh-exporter"}"
+docker_image_tag="${2:-"3.3.2"}"
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build bosh_exporter.go
+DOCKER_IMAGE_NAME=${docker_image_name} DOCKER_IMAGE_TAG=${docker_image_tag} make docker
+
+docker login
+docker tag "${docker_image_name}:${docker_image_tag}" "${docker_image_name}:${docker_image_tag}"
+docker push "${docker_image_name}:${docker_image_tag}"
---
+++ scripts/push-to-harbor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+docker_image_name="${1:-"malston/bosh-exporter"}"
+docker_image_tag="${2:-"3.3.2"}"
+project="${3:-"prometheus"}"
+docker_release_tag="${4:-"3.3.2"}"
+
+if [[ -z "${HARBOR_ADMIN_PASSWORD}" ]]; then
+  echo "Enter the password for the harbor administrator account: "
+  read -rs HARBOR_ADMIN_PASSWORD
+fi
+
+if [[ -z "${HARBOR_URL}" ]]; then
+  echo "Enter the dns hostname for harbor: (e.g. harbor.example.com)"
+  read -r HARBOR_URL
+fi
+
+docker pull "${docker_image_name}:${docker_image_tag}"
+docker login "https://${HARBOR_URL}" --username admin --password "${HARBOR_ADMIN_PASSWORD}"
+docker tag "${docker_image_name}:${docker_image_tag}" "${HARBOR_URL}/${project}/${docker_image_name}:${docker_release_tag}"
+docker push "${HARBOR_URL}/${project}/${docker_image_name}:${docker_release_tag}"
